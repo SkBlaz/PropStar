118907ae24b4a5b566ac412796f3b01b4fccee95
skblaz <skrljblaz@gmail.com>
2020-07-30 09:15:47 +0200

    Refactored the codebase

--- learning.py
+++ learning.py
@@ -11,8 +11,9 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import svm
 from sklearn.metrics import accuracy_score, f1_score
 
-def preprocess_and_split(X,num_fold=10, target_attribute = None):
-    
+
+def preprocess_and_split(X, num_fold=10, target_attribute=None):
+
     skf = StratifiedKFold(n_splits=num_fold)
     Y = X[target_attribute]
     if len(np.unique(Y)) > 40:
@@ -23,27 +24,39 @@ def preprocess_and_split(X,num_fold=10, target_attribute = None):
                 tmp.append(1)
             else:
                 tmp.append(0)
-        Y = tmp                 
+        Y = tmp
 
     for train_index, test_index in skf.split(X, Y):
-        yield train_index,test_index
+        yield train_index, test_index
 
-def svm_learner(train_features,train_classes):
-    clf = svm.SVC(kernel='rbf',C=10,gamma="scale")
-    clf.fit(train_features,train_classes)
+
+def svm_learner(train_features, train_classes):
+    clf = svm.SVC(kernel='rbf', C=10, gamma="scale")
+    clf.fit(train_features, train_classes)
     return clf
 
-def lr_learner (train_features,train_classes):
-    clf = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial').fit(train_features,train_classes)
+
+def lr_learner(train_features, train_classes):
+    clf = LogisticRegression(random_state=0,
+                             solver='lbfgs',
+                             multi_class='multinomial').fit(
+                                 train_features, train_classes)
     return clf
+
 
 class starspaceLearner:
-
     '''
     This is a simple wrapper for the starspace learner.
     '''
-    
-    def __init__(self,vb=False, binary="./starspace", tmp_folder ="tmp",epoch=5, dim=100, learning_rate = 0.01, neg_search_limit = 50, max_neg_samples = 10):
+    def __init__(self,
+                 vb=False,
+                 binary="./starspace",
+                 tmp_folder="tmp",
+                 epoch=5,
+                 dim=100,
+                 learning_rate=0.01,
+                 neg_search_limit=50,
+                 max_neg_samples=10):
         self.binary = binary
         self.tmp = tmp_folder
         self.epoch = epoch
@@ -51,7 +64,8 @@ class starspaceLearner:
         self.lr = learning_rate
         self.nsl = neg_search_limit
         self.nspb = max_neg_samples
-        self.parameter_string = "-epoch {} -negSearchLimit {} -maxNegSamples {} -lr {} -dim {}".format(self.epoch, self.nsl, self.nspb, self.lr, self.dim)
+        self.parameter_string = "-epoch {} -negSearchLimit {} -maxNegSamples {} -lr {} -dim {}".format(
+            self.epoch, self.nsl, self.nspb, self.lr, self.dim)
 
         print(self.parameter_string)
 
@@ -63,7 +77,7 @@ class starspaceLearner:
             number_of_rows = train_data.shape[0]
             for enx, el in enumerate(rows):
                 row_matrix[el].append(cols[enx])
-                
+
             input_text = []
             for j in range(number_of_rows):
                 if len(row_matrix[j]) == 0:
@@ -74,56 +88,64 @@ class starspaceLearner:
                 input_text.append(input_string)
             return input_text
         else:
-            labels = ["__label__"+str(x) for x in train_data]
-#            labels = "__label__"+train_data.astype(str).values
+            labels = ["__label__" + str(x) for x in train_data]
+            #            labels = "__label__"+train_data.astype(str).values
             return labels
 
-    def write_to_tmp(self,filedump,tag="train"):
-        
-        fx = open(self.tmp+"/"+tag+"_data.txt","w+")
+    def write_to_tmp(self, filedump, tag="train"):
+
+        fx = open(self.tmp + "/" + tag + "_data.txt", "w+")
         fx.write(filedump)
         fx.close()
 
-    def call_starspace_binary(self,train=True, output_model = "tmp/storedModel"):
+    def call_starspace_binary(self,
+                              train=True,
+                              output_model="tmp/storedModel"):
 
         if train:
             train_file = "./tmp/train_data.txt"
-            to_execute = "./starspace train {} -verbose 0 -trainFile ".format(self.parameter_string)+train_file+" -model "+output_model
+            to_execute = "./starspace train {} -verbose 0 -trainFile ".format(
+                self.parameter_string) + train_file + " -model " + output_model
             os.system(to_execute)
         else:
 
             test_file = "./tmp/test_data.txt"
             to_execute = """
             ./query_predict {modelfile} 1 < {testfile}
-            """.format(modelfile="tmp/storedModel",testfile="tmp/test_data.txt")
+            """.format(modelfile="tmp/storedModel",
+                       testfile="tmp/test_data.txt")
             output = subprocess.check_output(to_execute, shell=True)
             return output
-            
-    def fit(self,train_data,train_labels):
+
+    def fit(self, train_data, train_labels):
         train_text = self.data_to_text(train_data)
-        train_label_text = self.data_to_text(train_labels,label_tag=True)
+        train_label_text = self.data_to_text(train_labels, label_tag=True)
         total_data = []
         for enx, el in enumerate(train_text):
-            total_list = el+" "+ train_label_text[enx]
+            total_list = el + " " + train_label_text[enx]
             total_data.append(total_list)
         out_file = "\n".join(total_data)
         self.write_to_tmp(out_file)
         self.call_starspace_binary()
 
     def _cleanup(self):
-#        os.remove("./tmp/train_data.txt")
-#        os.remove("./tmp/test_data.txt")
-#        os.remove("./tmp/storedModel.tsv")
-#        os.remove("./tmp/storedModel")
+        #        os.remove("./tmp/train_data.txt")
+        #        os.remove("./tmp/test_data.txt")
+        #        os.remove("./tmp/storedModel.tsv")
+        #        os.remove("./tmp/storedModel")
         os.system("rm -rf ./tmp/*")
         time.sleep(3)
-        
-    def predict(self,test_data, return_scores = False, clean_tmp = False, return_int_predictions = True):
+
+    def predict(self,
+                test_data,
+                return_scores=False,
+                clean_tmp=False,
+                return_int_predictions=True):
 
         train_text = self.data_to_text(test_data)
         total_data = []
         for enx, el in enumerate(train_text):
-            total_list = el #+" "+ train_label_text[enx]
+            total_list = el  #+" "+ train_label_text[enx]
             total_data.append(total_list)
         out_file = "\n".join(total_data)
 
@@ -133,7 +155,7 @@ class starspaceLearner:
         for el in otpt.split("\\n"):
             if "Enter" in el and "__label__" in el:
                 if not return_scores:
-                    el1 = el.split(" ")[-2].replace("__label__","")
+                    el1 = el.split(" ")[-2].replace("__label__", "")
                 else:
                     el1 = float(el.split(" ")[-3].split("[")[1].split("]")[0])
                 els.append(el1)
--- vectorizers.py
+++ vectorizers.py
@@ -5,22 +5,22 @@ import scipy.sparse as sps
 import queue
 import tqdm
 import logging
-logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S')
 logging.getLogger().setLevel(logging.INFO)
 
+
 class conjunctVectorizer:
-
-    def __init__(self, max_atoms = (1,1), max_features = 10000, binary = False):
-
+    def __init__(self, max_atoms=(1, 1), max_features=10000, binary=False):
         """
         Instantiation class -> set default values + queue size.
         """
-        
+
         self.max_atoms = max_atoms
         self.max_features = max_features
         self.binary = binary
         self.overall_counts = {}
-        self.queue_maxsize = 20000 ## increase this for richer feature space
+        self.queue_maxsize = 20000  ## increase this for richer feature space
         self.selected_witems = queue.PriorityQueue(self.queue_maxsize)
         self.min_score = 0
         self.overall_witem_counts = {}
@@ -29,7 +29,7 @@ class conjunctVectorizer:
         """
         A standard fit method (sklearn-style). It operates on a list of sets.
         """
-        
+
         self.all_witems = set()
         for doc in witem_documents:
             for witem in doc:
@@ -38,16 +38,22 @@ class conjunctVectorizer:
                 else:
                     self.overall_counts[witem] += 1
 
-        for witem_order in range(self.max_atoms[0], self.max_atoms[1]+1):
+        for witem_order in range(self.max_atoms[0], self.max_atoms[1] + 1):
             logging.info("Now computing witem order {}".format(witem_order))
-            all_combinations_of_order = list(combinations(self.overall_counts.keys(), witem_order))
-            for candidate_feature in tqdm.tqdm(all_combinations_of_order, total = len(list(all_combinations_of_order))):
+            all_combinations_of_order = list(
+                combinations(self.overall_counts.keys(), witem_order))
+            for candidate_feature in tqdm.tqdm(
+                    all_combinations_of_order,
+                    total=len(list(all_combinations_of_order))):
                 if self.selected_witems.qsize() == self.queue_maxsize:
-                    logging.info("Max priority queue size achieved. Breaking ..")
+                    logging.info(
+                        "Max priority queue size achieved. Breaking ..")
                     break
-                
-                all_counts = [self.overall_counts[x] for x in candidate_feature]
-                score = 1/np.mean(all_counts)
+
+                all_counts = [
+                    self.overall_counts[x] for x in candidate_feature
+                ]
+                score = 1 / np.mean(all_counts)
                 priority_score = np.mean(all_counts)
                 self.overall_witem_counts[candidate_feature] = score
                 self.selected_witems.put((-priority_score, candidate_feature))
@@ -64,9 +70,8 @@ class conjunctVectorizer:
         if self.top_witems:
             names = [" AND ".join(x) for x in self.top_witems]
             return names
-            
-    def transform(self, witem_documents):
 
+    def transform(self, witem_documents):
         """
         Once fitted, this method transforms a list of witemsets to a sparse matrix.
         """
@@ -78,7 +83,7 @@ class conjunctVectorizer:
         doc_count = 0
         for idx, doc in enumerate(witem_documents):
             doc_count += 1
-            for enx, witemset in enumerate(self.top_witems):                
+            for enx, witemset in enumerate(self.top_witems):
                 if doc.intersection(set(witemset)):
                     query.append(idx)
                     key.append(enx)
@@ -88,14 +93,14 @@ class conjunctVectorizer:
                         values.append(self.overall_witem_counts[witemset])
 
         assert len(query) == len(key)
-        m = sps.csr_matrix((values, (query, key)), shape=(doc_count,self.max_features))
+        m = sps.csr_matrix((values, (query, key)),
+                           shape=(doc_count, self.max_features))
         return m
-                        
-    def fit_transform(self, witem_documents):    
 
+    def fit_transform(self, witem_documents):
         """
         Fit and transform in a single call.
         """
-        
+
         self.fit(witem_documents)
         return self.transform(witem_documents)
